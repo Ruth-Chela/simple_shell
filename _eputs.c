f7bdafa0368a5077406fcb1a6d7193ef32d81af7
ucheogbodo-25 <ucheogbodo25@gmail.com>
2023-05-23 07:43:11 -0700

    error tasks

---
+++ _eputs.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _eputs - outputs the input  str to the console
+ * @str: the str to be printed
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - Outputs the character 'c' to the standard error stream
+ * @c: the printable character
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and sets a corresponding  errno code
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - Writes the character 'c' to the specified fd
+ * @c: the printable character
+ * @fd: The fd to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and sets a corresponding errno code.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - Outputs the input  str to the console
+ * @str: the printable str
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of characters put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)ii
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
